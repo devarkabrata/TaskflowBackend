794623a89116e05272f07f127e795b5c26a225fe
devarkabrata <arkabrata.chandra@codeclouds.co.in>
2026-08-09 11:53:13 +0530

    routing key changed

--- Helpers/RoutingKeys.cs
+++ Helpers/RoutingKeys.cs
@@ -5,7 +5,7 @@ namespace TaskFlowBackend.Helpers
         public const string FromEmail = "try.soumyabroto@gmail.com";
         public const string TaskCreated = "email.task-created";
         public const string MemberAdded = "email.member-added";
-        public const string WorkspaceInvite = "email.workspace-invite";
+        public const string WorkspaceInvite = "email.invite-member";
         public const string TeamCreated = "email.team-created";
         public const string ForgotPassword = "email.forgot-password";
         public const string WelcomeEmail = "email.welcome";
