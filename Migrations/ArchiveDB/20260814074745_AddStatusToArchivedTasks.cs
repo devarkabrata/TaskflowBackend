42204db391512e7dcee78e16dfc9394d287318ea
devarkabrata <arkabrata.chandra@codeclouds.co.in>
2026-08-14 13:20:16 +0530

    status added in archieve task

---
+++ Migrations/ArchiveDB/20260814074745_AddStatusToArchivedTasks.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskFlowBackend.Migrations.ArchiveDB
+{
+    /// <inheritdoc />
+    public partial class AddStatusToArchivedTasks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "archived_tasks",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "archived_tasks");
+        }
+    }
+}
